2644e2b163fddad0dc08b8b4159a18dff939f53d
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-19 08:59:53 -0300

    onload por las dudas

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const btn = document.getElementById('btn');
-const inicioInput = document.getElementById('inicio');
-const finInput = document.getElementById('fin');
 
-btn.addEventListener('click', function () {
-    let a = parseInt(inicioInput.value);
-    let b = parseInt(finInput.value);
-    sumAll(a, b);
-});
+window.onload = () => {
+    const btn = document.getElementById('btn');
+    const inicioInput = document.getElementById('inicio');
+    const finInput = document.getElementById('fin');
 
+    btn.addEventListener('click', function () {
+        let a = parseInt(inicioInput.value);
+        let b = parseInt(finInput.value);
+        sumAll(a, b);
+    });
+};
 /**
  * Función que recibe dos números y muestra por consola
  * la suma de todos los numeros entre ellos.
